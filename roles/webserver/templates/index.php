4c0ad24e7b3e06ffcc4f356e622de26e55552bd5
root <root@desafio-ansible.us-central1-a.c.desafio-o2b.internal>
2019-08-10 21:11:59 +0000

    atualiza index php e playbook

---
+++ roles/webserver/templates/index.php
@@ -0,0 +1,17 @@
+</html>
+<head><title>EndPoint Desafio O2B</title></head>
+<body>
+<center>
+ <?php 
+  echo "<h2>#####By Daniel Souza ##### <br></h2>"; 
+ 
+  date_default_timezone_set('America/Sao_Paulo');
+  echo "<h3>";
+  echo "Data e hora: ".  date('d/m/Y - H:i');
+  echo "<br>";
+  echo "Rodando no diretorio: '".getcwd()."' ";
+  echo "</h3>";
+ ?>
+</center>
+</body>
+</html>
