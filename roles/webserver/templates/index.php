1a8d4645c79791622e4c01e80faa969cab21317a
Daniel Souza <danielsouza1405@gmail.com>
2019-08-11 08:42:17 -0300

    Update index.php

--- roles/webserver/templates/index.php
+++ roles/webserver/templates/index.php
@@ -2,13 +2,14 @@
 <head><title>EndPoint Desafio O2B</title></head>
 <body>
 <center>
- <?php 
-  date_default_timezone_set('America/Sao_Paulo');
+ <?php
   echo "<h3>";
+  echo "### Desafio DevOps O2B ###<br>";
+  date_default_timezone_set('America/Sao_Paulo');
   echo "Data e hora: ".  date('d/m/Y - H:i');
   echo "<br>";
-  echo "Rodando no diretorio: '".getcwd()."' ";
-  echo "### By Daniel Souza ### <br>";  
+  echo "Rodando no diretorio: '" .getcwd(). "'<br>";
+  echo "#### By Daniel Souza ####";
   echo "</h3>";
  ?>
 </center>
